ea6f7c8b25823f00a166104ecb9a96f3584d3939
marinoalessio <marino.alessio9599@gmail.com>
2022-03-15 16:48:18 +0100

    finalmentegit add .!

--- Lab1/ex_0.js
+++ Lab1/ex_0.js
@@ -1,1 +1,6 @@
-ciao
+"use strict";
+const words = ['i', 'spring', 'it', 'cat'];
+for(const word of words){
+    if(word.length > 1) console.log(word.slice(0, 2) + word.slice(-2));
+    else console.log('.');
+}
