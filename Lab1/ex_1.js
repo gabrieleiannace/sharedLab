b53552ac4707dfb4380f2e36c24735d9a1761ed4
marinoalessio <marino.alessio9599@gmail.com>
2022-03-15 18:20:15 +0100

    last commit

--- Lab1/ex_1.js
+++ Lab1/ex_1.js
@@ -1,5 +1,6 @@
 "use strict";
 
+const dayjs = require('dayjs');
 
 function Film(id, title, fav = false, date, rating){
     this.id = id;
@@ -19,16 +20,9 @@ function filmLibrary(){
     }
 }
 
-// Id: 1, Title: Pulp Fiction, Favorite: true, Watch date: March 10, 2022, Score: 5
-// Id: 2, Title: 21 Grams, Favorite: true, Watch date: March 17, 2022, Score: 4
-// Id: 5, Title: Shrek, Favorite: false, Watch date: March 21, 2022, Score: 3
-// Id: 3, Title: Star Wars, Favorite: false, Watch date: <not defined>, Score: <not defined>
-// Id: 4, Title: Matrix, Favorite: false, Watch date: <not defined>, Score: <not defined>
-
-
-const film1 = new Film(1, 'Pulp', true, "March 10, 2022", 5);
-const film2 = new Film(2, '21 Grams', true, 'March 17, 2022', 4);
-const film3 = new Film(5, 'Shrek', false, 'March 21, 2022', 3);
+const film1 = new Film(1, 'Pulp', true, dayjs('20211005'), 5);
+const film2 = new Film(2, '21 Grams', true, dayjs('20220120'), 4);
+const film3 = new Film(5, 'Shrek', false, dayjs('20211213'), 3);
 const film4 = new Film(3, 'Star Wars', false);
 const film5 = new Film(4, 'Matrix', false);
 
